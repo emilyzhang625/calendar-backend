509dc819141865c40e08e6f1dbb221c9df3d6123
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-07-07 17:56:51 -0700

    implement delete for backend

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const { v4: uuidv4 } = require('uuid');
 app.use(cors())
 app.use(express.json())
 
-
 let items = []
-
 
 app.get("/api/items", (request, response) => {
 	response.json(items)
 })
 
 app.get("/api/items/:id", (request, response) => {
-	const id = Number(request.params.id)
+	const id = (request.params.id)
 	const item = items.find(item => item.id === id)
 
 	if (item) {
@@ -29,9 +27,20 @@ app.get("/api/items/:id", (request, response) => {
 app.post("/api/items", (request, response) => {
 	const body = request.body
 	const item = {name: body.name, id: uuidv4(), year: body.year, month:body.month, day:body.day}
-	console.log(item)
 	items = items.concat(item)
 	response.json(item)
+})
+
+app.delete("/api/items/:id", (request,response) => {
+	const id = (request.params.id)
+	const item = items.find(item => item.id === id)
+	items = items.filter(item => item.id !== id)
+	if (item) {
+		response.json(item);
+	}
+		else {
+	response.status(204).end();
+		}
 })
 
 const PORT = 3001
