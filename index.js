9f1d2bf5e52f916aaad86b005363d72898e08799
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-07-20 12:14:34 -0700

    changed index.js file to get any path and redirect to index.html using path, not sure about this

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ require('dotenv').config()
 
 const express = require("express")
 const cors = require("cors")
+const path = require('path');
 const app = express()
 const User = require('./models/user')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.get('/api/users', (request, response) => {
 	User.find({}).then(users => {
